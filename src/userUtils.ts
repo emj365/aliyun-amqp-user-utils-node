cf56e8fc571761c5c27ee4a182c7b50ec255fa6f
Michael JIN <emj365@gmail.com>
2019-04-01 15:58:59 +0800

    refactor string join

--- src/userUtils.ts
+++ src/userUtils.ts
@@ -3,9 +3,9 @@ import * as crypto from 'crypto'
 const ACCESS_FROM_USER = 0
 
 export const getUserName = (ak: string, resourceOwnerId: number): string => {
-  return Buffer.from(`${ACCESS_FROM_USER}:${resourceOwnerId}:${ak}`).toString(
-    'base64'
-  )
+  return Buffer.from(
+    [ACCESS_FROM_USER, resourceOwnerId, ak].join(':')
+  ).toString('base64')
 }
 
 export const getPassword = (sk: string): string => {
